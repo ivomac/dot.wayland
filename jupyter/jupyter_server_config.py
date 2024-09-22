b45b05c909bfe3a8585e63d16e23aaf1c2507a43
Ivo Maceira <ivomaceira@gmail.com>
2024-09-23 01:04:39 +0200

    rename jupyter config
    update jupyter config to 2.0

--- jupyter/jupyter_server_config.py
+++ jupyter/jupyter_server_config.py
@@ -1,6 +1,9 @@
 
 c = get_config()
 
+c.PasswordIdentityProvider.password_required = True
+c.PasswordIdentityProvider.allow_password_change = False
+
 c.LabApp.open_browser = False
 
 c.LabServerApp.open_browser = False
@@ -12,11 +15,8 @@ c.ServerApp.port = 8888
 c.ServerApp.open_browser = False
 c.ServerApp.webbrowser_open_new = 2
 
-c.ServerApp.password_required = True
-
 c.ServerApp.allow_root = False
 c.ServerApp.allow_remote_access = False
-c.ServerApp.allow_password_change = False
 c.ServerApp.allow_unauthenticated_access = False
 
 c.ServerApp.terminals_enabled = False
